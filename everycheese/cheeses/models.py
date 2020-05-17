6fdf9eb2227d4e907df66420696116e58e098ba2
Venkat <vkrish83@gmail.com>
2020-05-17 05:47:08 +0530

    Add creator field to Cheese model and custom migrations

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -3,6 +3,7 @@ from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.conf import settings
 
 class Cheese(TimeStampedModel):
     name = models.CharField("Name of the Cheese", max_length=255)
@@ -20,6 +21,11 @@ class Cheese(TimeStampedModel):
 
     country_of_origin = CountryField("Country of Origin", blank=True)
 
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
+    )
     def get_absolute_url(self):
         return reverse("cheeses:detail", kwargs={"slug": self.slug})
     
